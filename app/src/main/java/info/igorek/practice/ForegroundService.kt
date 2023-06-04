95c9b429241430bb2c9eb74c55302c5a33bd43d5
Ihar Leshchanka <ihar.leshchanka@gmail.com>
2023-06-04 23:12:55 +0200

    Init foreground service

---
+++ app/src/main/java/info/igorek/practice/ForegroundService.kt
@@ -0,0 +1,122 @@
+package info.igorek.practice
+
+import android.app.Notification
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import com.google.android.exoplayer2.ui.PlayerNotificationManager.MediaDescriptionAdapter
+import com.google.android.exoplayer2.ui.PlayerNotificationManager.NotificationListener
+import com.google.android.exoplayer2.util.NotificationUtil.IMPORTANCE_HIGH
+
+class ForegroundService : Service() {
+
+    companion object {
+        private const val NOTIFICATION_ID = 123
+        private const val CHANNEL_ID = "channel_id"
+    }
+
+    private lateinit var player: ExoPlayer
+    private lateinit var notificationManager: PlayerNotificationManager
+
+    private val descriptionAdapter = object : MediaDescriptionAdapter {
+        override fun getCurrentContentTitle(player: Player): CharSequence {
+            return player.currentMediaItem?.mediaMetadata?.title.toString()
+        }
+
+        override fun createCurrentContentIntent(player: Player): PendingIntent? {
+            val openApp = Intent(applicationContext, MainActivity::class.java)
+            return PendingIntent.getActivity(
+                applicationContext,
+                0,
+                openApp,
+                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
+            )
+        }
+
+        override fun getCurrentContentText(player: Player): CharSequence? {
+            return player.currentMediaItem?.mediaMetadata?.description
+        }
+
+        override fun getCurrentLargeIcon(player: Player, callback: PlayerNotificationManager.BitmapCallback): Bitmap? {
+            return null
+        }
+    }
+
+    private val notificationListener = object : NotificationListener {
+        override fun onNotificationCancelled(notificationId: Int, dismissedByUser: Boolean) {
+            stopSelf()
+            super.onNotificationCancelled(notificationId, dismissedByUser)
+        }
+
+        override fun onNotificationPosted(notificationId: Int, notification: Notification, ongoing: Boolean) {
+            startForeground(notificationId, notification)
+            super.onNotificationPosted(notificationId, notification, ongoing)
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        player = ExoPlayer.Builder(this).build()
+
+        notificationManager = PlayerNotificationManager.Builder(
+            this,
+            NOTIFICATION_ID,
+            CHANNEL_ID,
+        )
+            .setChannelImportance(IMPORTANCE_HIGH)
+            .setMediaDescriptionAdapter(descriptionAdapter)
+            .setNotificationListener(notificationListener)
+
+            .setSmallIconResourceId(R.drawable.ic_launcher_foreground)
+            .setNextActionIconResourceId(R.drawable.ic_launcher_foreground)
+            .setPreviousActionIconResourceId(R.drawable.ic_launcher_foreground)
+            .setPlayActionIconResourceId(R.drawable.ic_launcher_foreground)
+
+            .setChannelDescriptionResourceId(R.string.channel_desc)
+            .setChannelNameResourceId(R.string.channel_name)
+            .build()
+
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(C.USAGE_MEDIA)
+            .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+            .build()
+
+        val mediaItem = MediaItem.fromUri(Uri.parse("android.resource://$packageName/raw/david_cutter_music_ack_woi"))
+        player.apply {
+            setAudioAttributes(audioAttributes, true)
+            setMediaItem(mediaItem)
+            prepare()
+            playWhenReady = true
+        }
+
+        notificationManager.apply {
+            setPlayer(player)
+            setPriority(NotificationCompat.PRIORITY_MAX)
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        notificationManager.setPlayer(null)
+        player.release()
+        super.onDestroy()
+    }
+}
--- app/src/main/java/info/igorek/practice/MainActivity.kt
+++ app/src/main/java/info/igorek/practice/MainActivity.kt
@@ -1,11 +1,31 @@
 package info.igorek.practice
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.util.Util
+import info.igorek.practice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        with(binding) {
+            buttonStart.setOnClickListener {
+                Util.startForegroundService(
+                    this@MainActivity,
+                    Intent(this@MainActivity, ForegroundService::class.java)
+                )
+            }
+            buttonStop.setOnClickListener {
+
+            }
+        }
     }
 }
